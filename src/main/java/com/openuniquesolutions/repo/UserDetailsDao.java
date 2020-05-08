076c94a8d707cbb6fd25678ef0f57ca66cc51f07
ayush <ayushnita.ece@gmail.com>
2020-05-08 20:45:18 +0530

    Add optional in return type

--- src/main/java/com/openuniquesolutions/repo/UserDetailsDao.java
+++ src/main/java/com/openuniquesolutions/repo/UserDetailsDao.java
@@ -1,5 +1,7 @@
 package com.openuniquesolutions.repo;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.openuniquesolutions.model.UserModel;
 
 @Repository
 public interface UserDetailsDao extends MongoRepository<UserModel, String> {
-	UserModel findByUserId(String userId);
+	Optional<UserModel> findByUserId(String userId);
 }
